4cf2aa44bf94ca9bd53bb881672b0319fb1800e5
Lauri Vastisson <94855381+lvastisson@users.noreply.github.com>
2022-03-01 08:10:26 +0200

    working

--- stacker.js
+++ stacker.js
@@ -5,16 +5,24 @@ var spacePressed = false;
 var spaceReleased = true;
 var barSize = 200;
 var barPos = 400;
-var barHeight = 700;
+var barHeight = 1100;
 var barDir = 1;
 var barSpeed = 10;
 
-var bars = [new Bar(400, 750,200)];
+var bars = [new Bar(400, 1150,200)];
+
+var colors = ['white', 'red', 'gray', 'yellow', 'orange', 'purple', 'green', 'pink'];
 
 var theGameLoop;
 var gameLoopOn = true;
 
 function runGame() {
+
+    for (let index = 0; index < 100; index++) {
+        
+        console.log(getLoopingIndex(index, colors.length - 1));
+    }
+
     initializeGame();
     gameLoop();
 }
@@ -44,7 +52,7 @@ function gameLoop() {
     if(spacePressed) {
         var posDiff = barPos - bars[bars.length - 1].pos
 
-        if (posDiff > barSize / 2 + bars[bars.length - 1].size / 2) {
+        if (Math.abs(posDiff) >= barSize / 2 + bars[bars.length - 1].size / 2) {
             callGameOver();
         }
 
@@ -66,22 +74,27 @@ function gameLoop() {
 }
 
 function render() {
-    ctx.clearRect(0, 0, 800, 800);
+    ctx.clearRect(0, 0, 800, 1200);
 
     ctx.fillStyle = "black";
-    ctx.fillRect(0, 0, 800, 800);
+    ctx.fillRect(0, 0, 800, 1200);
 
-    ctx.fillStyle = "white";
     for (let i = 0; i < bars.length; i++) {
         const elem = bars[i];
+
+        ctx.fillStyle = colors[getLoopingIndex(i, colors.length)];
         
         ctx.fillRect(elem.pos - (elem.size / 2), elem.height, elem.size, 50)
     }
 
-    ctx.fillStyle = "white";
+    ctx.fillStyle = colors[getLoopingIndex(bars.length, colors.length)];
     ctx.fillRect(barPos - (barSize / 2), barHeight, barSize, 50);
 
     ctx.stroke();
+}
+
+function getLoopingIndex(index, maxIndex) {
+    return index - (Math.floor(index / maxIndex) * maxIndex);
 }
 
 function callGameOver() {
